74cff204693b043f1bdbb15f8a5e674e1fa7751f
Colin Cuthrell-Tompkins <colin@tyrannosaurustech.com>
2023-03-20 15:49:30 -0400

    feat: Add styled components library, react hook forms, and build form foundation

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-import "./App.css";
 import { CharacterProfile } from "./app/CharacterProfile";
 import { getBattleNetAccessToken } from "./app/apiCalls";
 import { CharacterPortrait } from "./app/CharacterPortrait";
